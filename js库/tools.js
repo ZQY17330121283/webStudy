5c0b4fdbcdeb4d07ff668795dbe8b9c3a820a7da
熬夜小痴呆 <63138606+ZQY17330121283@users.noreply.github.com>
2020-08-29 20:21:22 +0800

    2020-8-29

--- js库/tools.js
+++ js库/tools.js
@@ -1,61 +1,72 @@
 //打印当前年月日分秒
 function myDate() {
   var date = new Date();
-  console.log("当前是" + date.getFullYear() + "年" + (date.getMonth() + 1)
-  + "月" + date.getDate() + "日" + date.getHours() + "时" + date.getMinutes() + "分" + date.getSeconds() + "秒");
+  console.log(
+    "当前是" +
+      date.getFullYear() +
+      "年" +
+      (date.getMonth() + 1) +
+      "月" +
+      date.getDate() +
+      "日" +
+      date.getHours() +
+      "时" +
+      date.getMinutes() +
+      "分" +
+      date.getSeconds() +
+      "秒"
+  );
 }
 
 // 返回滚动条滚动的距离 向IE8及IE8以下兼容
-function getScrollOffset(){
-  if(window.pageXOffset){
+function getScrollOffset() {
+  if (window.pageXOffset) {
     return {
-      x : window.pageXOffset,
-      y : window.pageYOffset
-    }
-    }else{
-      return{
-        x : document.body.scrollLeft + document.documentElement.scrollLeft,
-        y : document.body.scrollTop + document.documentElement.scrollTop
-      }
+      x: window.pageXOffset,
+      y: window.pageYOffset,
+    };
+  } else {
+    return {
+      x: document.body.scrollLeft + document.documentElement.scrollLeft,
+      y: document.body.scrollTop + document.documentElement.scrollTop,
+    };
   }
 }
 
 //查看视图的尺寸
-function getViewportOffset(){
-  if(window.innerWidth){
-    return{
-      w : window.innerWidth,
-      h : window.innerHeight
-    }
-  }else{
-    if(document.compatMode === "BackCompat"){
-      return{
-        w : document.body.clientWidth,
-        h : document.body.clientHeight
-      }
-    }else{
-      return{
-        w : document.documentElement.clientWidth,
-        h : document.documentElement.clientHeight
-      }
+function getViewportOffset() {
+  if (window.innerWidth) {
+    return {
+      w: window.innerWidth,
+      h: window.innerHeight,
+    };
+  } else {
+    if (document.compatMode === "BackCompat") {
+      return {
+        w: document.body.clientWidth,
+        h: document.body.clientHeight,
+      };
+    } else {
+      return {
+        w: document.documentElement.clientWidth,
+        h: document.documentElement.clientHeight,
+      };
     }
   }
 }
-
-
 
 //绑定事件，第一个值传dom   第二个值传事件类型   第三个值传处理函数
 // addEvent(demo, "click", test);
 //   function test(){
 //     console.log("a");
 //   };
-function addEvent(elem,type,handle) {
+function addEvent(elem, type, handle) {
   if (elem.addEventListener) {
-    elem.addEventListener(type,handle,false);
+    elem.addEventListener(type, handle, false);
   } else if (elem.attachEvent) {
-    elem.attachEvent ("on" + type, function (){
+    elem.attachEvent("on" + type, function () {
       handle.call(elem);
-    })
+    });
   } else {
     elem["on" + type] = handle;
   }
@@ -65,7 +76,7 @@ function addEvent(elem,type,handle) {
 function stopBubble(event) {
   if (event.stopPropagation) {
     event.stopPropagation();
-  }else {
+  } else {
     event.cancelBubble = true;
   }
 }
